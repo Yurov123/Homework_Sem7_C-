5dd4aafb81e3d3de7626be44333ce2c46a5f78f9
Konstantin <constantin8271@gmail.com>
2022-02-24 19:26:31 +0300

    Task_53

---
+++ Task_53/Program.cs
@@ -0,0 +1,33 @@
+﻿// В двумерном массиве показать позиции числа, заданного пользователем, или указать, что такого элемента нет.
+
+Console.Write("Enter the number of lines: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Enter number of columns: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Enter a number to search: "); // Введите число для поиска
+int number = int.Parse(Console.ReadLine());
+int[,] array = new int[m, n];
+Console.WriteLine();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(0, 10);
+        Console.Write(array[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+bool flag = true;
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (array[i,j] == number)
+        {
+            flag = false;
+            Console.Write($"({i},{j})  ");
+        }
+    }
+}
+if (flag)  Console.WriteLine("There is no such element.");
